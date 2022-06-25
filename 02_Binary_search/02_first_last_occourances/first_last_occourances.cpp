187f214a4e5b14c8b126898b47e5e22f79ec5a81
Saurabh Singh <saurabhsinghggu@gmail.com>
2022-06-25 21:32:42 +0530

    binary search

--- 02_Binary_search/02_first_last_occourances/first_last_occourances.cpp
+++ 02_Binary_search/02_first_last_occourances/first_last_occourances.cpp
@@ -7,6 +7,7 @@ int left_occourances(int arr[], int size, int key)
 {
     int start = 0, end = size - 1;
     int mid = start + (end - start) / 2;
+
     int ans = -1;
     while (end >= start)
     {
@@ -55,17 +56,17 @@ int right_occourances(int arr[], int size, int key)
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 3, 3};
+    int arr[4] = {1, 1, 2, 3};
     cout << "Left Index of"
          << " "
          << "3"
          << " "
          << "is"
-         << " " << left_occourances(arr, 5, 3) << endl;
+         << " " << left_occourances(arr, 5, 1) << endl;
     cout << "Right Index of"
          << " "
          << "3"
          << " "
          << "is"
-         << " " << right_occourances(arr, 5, 3);
+         << " " << right_occourances(arr, 5, 1);
 }
